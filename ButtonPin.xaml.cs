3e2c455b746999926d566d2cd740603b1cf72807
gmastergreatee <g77471164@gmail.com>
2018-04-10 17:02:33 +0530

    Alpha version released

---
+++ ButtonPin.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ASN
+{
+    /// <summary>
+    /// Interaction logic for ButtonPin.xaml
+    /// </summary>
+    public partial class ButtonPin : UserControl
+    {
+
+        public ButtonPin()
+        {
+            InitializeComponent();
+            DeActivate();
+        }
+
+        public void Activate()
+        {
+            img_blue_pin.Visibility = Visibility.Visible;
+            img_white_pin.Visibility = Visibility.Hidden;
+        }
+
+        public void DeActivate()
+        {
+            img_blue_pin.Visibility = Visibility.Hidden;
+            img_white_pin.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ASN
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isOnTop = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn_pin.DeActivate();
+        }
+
+        private void Header_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Environment.Exit(0);
+        }
+
+        private void toggleTop_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isOnTop)
+            {
+                isOnTop = true;
+                this.Topmost = true;
+                btn_pin.Activate();
+            }
+            else
+            {
+                isOnTop = false;
+                this.Topmost = false;
+                btn_pin.DeActivate();
+            }
+        }
+    }
+}
